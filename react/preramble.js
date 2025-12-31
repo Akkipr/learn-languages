53bfa722d0ab8bef3a6c0a33020a90f3a2d97df2
Akkipr <akshayanpraba6@gmail.com>
2025-12-31 00:10:40 -0500

    refactor: added objects and copying

--- react/preramble.js
+++ react/preramble.js
@@ -28,7 +28,21 @@ let age = 10;
 let name = age > 10 && "Pedro"; // if age > 10, let name be Pedro
 let name2 = age > 10 || "Pedro"; // if age is not > 10, let name be Pedro
 let name3 = age > 10 ? "Pedro" : "Jack"; // if age > 10, let name be Pedro, else Jack (we saw this in Python)
-
 const Component = () => {
     return age > 10 ? <div> Pedro </div> : <div> Jack </div>
-}
+};
+
+// Objects
+const person = {
+    name: "pedro",
+    age,                    // since we have the variable age on line 27, we don't need to repeat it. "age : age," also works, or "age,""
+    isSleeping: false,
+};
+const { firstname, age1, isMarried} = person;
+
+// Copy objects from one to another (spread operator)
+const person2 = {...person, name: "Jack"};
+
+// Copy arrays
+const lists = [10,20,30];
+const list2 = [...lists, 40];
